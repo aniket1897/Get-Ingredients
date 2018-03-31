b2d0a6729784422f330c5c5c4537cc9c021f5394
aniket1897 <paianiket4@gmail.com>
2018-03-31 20:08:56 +0530

    Changed color of error message

--- php/check1.php
+++ php/check1.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
-$db = mysqli_connect('localhost', 'root', '', 'registration1');
+$db = mysqli_connect('localhost', 'root', '', 'Get-Recipe');
 $email=$_POST['email'];
   $user_check_query = "SELECT * FROM users WHERE email='$email' LIMIT 1";
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
   if ($user['email'] === $email) {
-       echo "Email Already Exist";
+       echo "<p style='color:red;'>Email Already Exists</p>";
     }
     else
     {
